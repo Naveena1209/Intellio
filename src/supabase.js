2653b5005767fa723c844a3cb50038c31cfa5efe
Naveena1209 <naveenasenthil0307@gmail.com>
2026-02-25 16:19:10 +0530

    m

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://fzqjmyrfqxcujndujlzo.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_F2bglAlKIoSKFQEvjDCngg_exHgVVfZ";
+const REACT_APP_SUPABASE_URL = "https://fzqjmyrfqxcujndujlzo.supabase.co";
+const REACT_APP_SUPABASE_ANON_KEY = "sb_publishable_F2bglAlKIoSKFQEvjDCngg_exHgVVfZ";
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient(REACT_APP_SUPABASE_URL, REACT_APP_SUPABASE_ANON_KEY);
